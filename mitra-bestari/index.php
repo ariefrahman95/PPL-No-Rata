aefc3b989e278e78b81fe167368a9d0a8995094c
Destra Bintang Perkasa <destra.bintang.perkasa@gmail.com>
2014-05-07 17:50:47 +0700

    database baru (lagi)

--- mitra-bestari/index.php
+++ mitra-bestari/index.php
@@ -36,19 +36,27 @@
 						<h2><i class="icon-filter"></i>Journals To Review</h2>
 					</div>
 					<div class="box-content">
+					<form class="form-horizontal">
+					<fieldset>
+					<div class="control-group" id="group2">
 						<?php include "database_connection.php";
 							$query_jurnal = "select id, judul from jurnal where status='2'";
 							$hasil = mysql_query($query_jurnal,$db);
+							$count = mysql_num_rows($hasil);
+							$i=0;
 							while($row = mysql_fetch_array($hasil)){
-								echo'<label class="checkbox">';
-								echo'<input type="checkbox" id="Checkbox'.$row['id'].'" value="option1"> <a href="form_penilaian.php?id='.$row["id"].'" target="_blank" >'.$row["judul"].'</a>';
-								 	echo'</label>';
+								echo'<label class="checkbox" id="Checkbok'.$i.'b">';
+								echo'<input type="checkbox" id="Checkbox'.$i.'b"  name="Checkbox'.$i.'b" value="'.$row['id'].'"> <a href="download_jurnal.php?id='.$row["id"].'">'.$row["judul"].'</a>';
+								echo'</label>';
 							}
 						?>
+					</div>
+					</fieldset>
+					</form>
 						<div class="form-actions" align="center">
-							<button type="submit" class="btn">Reject</button>
+							<button type="submit" class="btn" onclick="reject_checked()" >Reject</button>
 							<button type="submit" class="btn">Accept with Revision</button>
-							<button type="submit" class="btn">Accept</button>
+							<button type="submit" class="btn" onclick="accept_checked()" >Accept</button>
 						</div>
 					</div>
 				</div><!--/span-->
@@ -61,21 +69,13 @@
 					<div class="box-content">
 						<form class="form-horizontal">
 							<fieldset>
-								<div class="control-group">
-									<?php
-											$query_jurnal = "select id, judul from jurnal where status='0'";
-											$hasil = mysql_query($query_jurnal,$db);
-											while($row = mysql_fetch_array($hasil)){
-												echo'<label class="checkbox">';
-												echo'<input type="checkbox" id="Checkbox'.$row['id'].'" value="option1"> <a href="form_penilaian.php?id='.$row["id"].'" target="_blank" >'.$row["judul"].'</a>';
-								  				echo'</label>';
-											}
-									?>
+								<div class="control-group" id="group1">
+									
 							  	</div>
 							</fieldset>
 						</form>
 						<div class="form-actions" align="center">
-							<button type="submit" class="btn btn-danger">Undo Reject</button>
+							<button type="submit" class="btn btn-danger" onclick="undo_reject()">Undo Reject</button>
 						</div>
 					</div>
 				</div><!--/span-->
@@ -86,16 +86,7 @@
 					<div class="box-content">
 						<form class="form-horizontal">
 							<fieldset>
-								<div class="control-group">
-								<?php
-											$query_jurnal = "select id, judul from jurnal where status='3'";
-											$hasil = mysql_query($query_jurnal,$db);
-											while($row = mysql_fetch_array($hasil)){
-												echo'<label class="checkbox">';
-												echo'<input type="checkbox" id="Checkbox'.$row['id'].'" value="option1"> <a href="form_penilaian.php?id='.$row["id"].'" target="_blank" >'.$row["judul"].'</a>';
-								  				echo'</label>';
-											}
-									?>
+								<div class="control-group" id="group4">
 								</div>
 							</fieldset>
 						</form>
@@ -111,21 +102,12 @@
 					<div class="box-content">
 						<form class="form-horizontal">
 							<fieldset>
-								<div class="control-group">
-								<?php
-											$query_jurnal = "select id, judul from jurnal where status='4'";
-											$hasil = mysql_query($query_jurnal,$db);
-											while($row = mysql_fetch_array($hasil)){
-												echo'<label class="checkbox">';
-												echo'<input type="checkbox" id="Checkbox'.$row['id'].'" value="option1"> <a href="form_penilaian.php?id='.$row["id"].'" target="_blank" >'.$row["judul"].'</a>';
-								  				echo'</label>';
-											}
-									?>
+								<div class="control-group" id="group3">
 								</div>
 							</fieldset>
 						</form>
 						<div class="form-actions" align="center">
-							<button type="submit" class="btn btn-danger">Undo Accept</button>
+							<button type="submit" class="btn btn-danger" onclick="undo_accept()">Undo Accept</button>
 						</div>
 					</div>
 				</div><!--/span-->
@@ -135,8 +117,8 @@
 
 			<fieldset>
 				<div class="form-actions">
-					<button type="submit" class="btn btn-primary"><i class="icon-ok"> </i> Apply</button>
-					<button type="reset" class="btn">Cancel</button>
+					<button type="submit" class="btn btn-primary" onclick="apply()"><i class="icon-ok"> </i> Apply</button>
+					<button type="reset" class="btn" onclick="cancel()">Cancel</button>
 				</div>
 			</fieldset>
 					<!-- content ends -->
@@ -155,4 +137,94 @@
 		}
 	</script>
 </body>
+<script>
+	function reject_checked(){
+		var i;
+		for(i=0; i<<?php echo $count;?>; i++){
+			if((document.getElementById("Checkbox"+i+"b").checked)&&(document.getElementById("Checkbok"+i+"b").parentNode==document.getElementById("group2"))){
+				//alert(document.getElementById("Checkbox"+i+"b").value);
+				document.getElementById("group1").appendChild(document.getElementById("Checkbok"+i+"b"));
+			}
+		}
+	}
+	function undo_reject(){
+		var i;
+		for(i=0; i<<?php echo $count;?>; i++){
+			if((document.getElementById("Checkbox"+i+"b").checked)&&(document.getElementById("Checkbok"+i+"b").parentNode==document.getElementById("group1"))){
+				//alert(document.getElementById("Checkbox"+i+"b").value);
+				document.getElementById("group2").appendChild(document.getElementById("Checkbok"+i+"b"));
+			}
+		}
+	}
+	function accept_checked(){
+		var i;
+		for(i=0; i<<?php echo $count;?>; i++){
+			if((document.getElementById("Checkbox"+i+"b").checked)&&(document.getElementById("Checkbok"+i+"b").parentNode==document.getElementById("group2"))){
+				//alert(document.getElementById("Checkbox"+i+"b").value);
+				document.getElementById("group3").appendChild(document.getElementById("Checkbok"+i+"b"));
+			}
+		}
+	}
+	function undo_accept(){
+		var i;
+		var count = <?php echo $count;?>;
+		for(i=0; i<<?php echo $count;?>; i++){
+			if((document.getElementById("Checkbox"+i+"b").checked)&&(document.getElementById("Checkbok"+i+"b").parentNode==document.getElementById("group3"))){
+				//alert(document.getElementById("Checkbox"+i+"b").value);
+				document.getElementById("group2").appendChild(document.getElementById("Checkbok"+i+"b"));
+			}
+		}
+	}
+	function cancel(){
+		window.location="journal_selection.php";
+	}
+	/*function apply(){
+		var xmlhttp;
+		var state = 0;
+		if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		}
+		else{// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	    }
+		var i;
+		var reject = "";
+		var accept = "";
+		var count = <?php echo $count;?>;
+		for(i=0; i<count; i++){
+			if((document.getElementById("Checkbox"+i+"b").checked)&&(document.getElementById("Checkbok"+i+"b").parentNode==document.getElementById("group3"))){
+				accept = accept + "&id" + i + "=" + document.getElementById("Checkbox"+i+"b").value;
+			}
+			else if((document.getElementById("Checkbox"+i+"b").checked)&&(document.getElementById("Checkbok"+i+"b").parentNode==document.getElementById("group1"))){
+				reject = reject + "&id" + i + "=" + document.getElementById("Checkbox"+i+"b").value;
+			}			
+		}
+		if(accept.length!=0){
+			xmlhttp.open("POST","send_to_mibes.php",true);
+			xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+			xmlhttp.send("count="+count+accept);
+			//alert("send_to_mibes" + document.getElementById("Checkbox"+i+"b").value);
+			xmlhttp.onreadystatechange = function () {
+				if (xmlhttp.readyState==4 && xmlhttp.status==200){
+					state++;
+				}
+			}
+		}
+		
+		if(reject.length!=0){
+			xmlhttp.open("POST","reject.php",true);
+			xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+			xmlhttp.send("count="+count+reject);
+			//alert("reject" + document.getElementById("Checkbox"+i+"b").value);
+			xmlhttp.onreadystatechange = function () {
+				if (xmlhttp.readyState==4 && xmlhttp.status==200){
+					state++;
+				}
+			}
+		}
+		
+		//setTimeout(cancel, 2000);
+		
+	}*/
+</script>
 </html>
